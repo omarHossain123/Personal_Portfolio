f992801a33a2a22ed36d92c3cd0b9ca706619cd9
o.hossain <omarhossain1610@gmail.com>
2024-10-16 23:54:38 -0400

    Door epic

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,10 +5,26 @@ import Section from './Section';
 const Map = () => {
   return (
     <div className="map">
+      {/* Character in the map */}
       <Character />
-      <Section name="Experience" position={{ top: '35%', left: '15%' }} />
-      <Section name="Projects" position={{ top: '35%', left: '45%' }} />
-      <Section name="Education" position={{ top: '35%', left: '75%' }} />
+
+
+      {/* New Labels for each door */}
+      <div className="door-label experience-label" style={{ top: '30%', left: '25%' }}>
+        Experience
+      </div>
+      <div className="door-label projects-label" style={{ top: '30%', left: '48.5%' }}>
+        Projects
+      </div>
+      <div className="door-label education-label" style={{ top: '30%', left: '72%' }}>
+        Education
+      </div>
+      <div className="door-label volunteer-label" style={{ top: '30%', left: '83%' }}>
+        Volunteer
+      </div>
+      <div className="door-label hobbies-label" style={{ top: '30%', left: '11.5%' }}>
+        Hobbies
+      </div>
     </div>
   );
 };
